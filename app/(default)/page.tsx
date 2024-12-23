6d70c080e2add5ebbbae2406fc3ad2a492e72424
videovulture <30363299+videovulture@users.noreply.github.com>
2024-12-22 17:41:57 -0800

    main

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -8,10 +8,12 @@ import BusinessCategories from "@/components/business-categories";
 import FeaturesPlanet from "@/components/features-planet";
 import LargeTestimonial from "@/components/large-testimonial";
 import Cta from "@/components/cta";
+import Banner2 from "@/components/banner2";
 
 export default function Home() {
   return (
     <>
+      <Banner2 />
       <Hero />
       <BusinessCategories />
       <FeaturesPlanet />
